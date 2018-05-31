38c16f6cbacd101571ebda040fcea890563ed5c9
史华力 <wb-shl391697@alibaba-inc.com>
2018-05-31 15:23:15 +0800

    learn redux

---
+++ src/Counter.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+// Add this function:
+function mapStateToProps(state) {
+  console.log('state :', state);
+  return {
+    count: state.count
+  };
+}
+
+class Counter extends React.Component {
+  increment = () => {
+    this.props.dispatch({ type: 'INCREMENT' });
+  }
+
+  decrement = () => {
+    this.props.dispatch({ type: 'DECREMENT' });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Counter</h2>
+        <div>
+          <button onClick={this.decrement}>-</button>
+          <span>{this.props.count}</span>
+          <button onClick={this.increment}>+</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+// Then replace this:
+// export default Counter;
+
+// With this:
+export default connect(mapStateToProps)(Counter);
